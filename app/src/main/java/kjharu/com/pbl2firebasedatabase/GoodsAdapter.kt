5d0660a9f7f5bddee50c5ac38ecf97a314c961b2
HoYoon-Lee <happylhy0630@naver.com>
2019-10-29 15:02:48 +0900

    add category

---
+++ app/src/main/java/kjharu/com/pbl2firebasedatabase/GoodsAdapter.kt
@@ -0,0 +1,8 @@
+package kjharu.com.pbl2firebasedatabase
+
+import android.content.Context
+import android.widget.ArrayAdapter
+
+class GoodsAdaptercontext(context: Context,resource: Int,item: Array<String>) : ArrayAdapter<String>(context,resource,item) {
+
+}
